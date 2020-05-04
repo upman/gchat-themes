2d548d09f74beb8c095bd394e6a7313bed0cfc18
rlazar <ravi.kumar.lazar@zalando.de>
2020-05-05 00:17:30 +0200

    Cleanup

--- ts-built/src/themes/index.js
+++
@@ -1,9 +0,0 @@
-import dark from './dark';
-import defaultMode from './default';
-import slackTheme from './slack';
-export default {
-    // The keys here are used as copy in the select box in popup.ts
-    'Dark Mode': dark,
-    'Default Theme': defaultMode,
-    'Slack': slackTheme,
-};
